746897391a1b9bf8933a80b938e8da5fa76fa8a5
Benjamin Saunders <ben.e.saunders@gmail.com>
2016-11-13 12:47:01 -0800

    Report dirtiness and rebuild when needed

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,46 @@ use std::{io, env};
 use std::io::Write;
 use std::path::PathBuf;
 use std::fs::File;
+use std::path::Path;
 
 use git2::{Repository, Oid};
 
 use errors::*;
 
+#[cfg(unix)]
+fn write_path<W: Write, P: AsRef<Path>>(out: &mut W, path: P) -> io::Result<usize> {
+    use std::os::unix::ffi::OsStrExt;
+
+    let path = path.as_ref().as_os_str().as_bytes();
+    out.write(path)
+}
+
+#[cfg(not(unix))]
+fn write_path<W: Write, P: AsRef<Path>>(out: &mut W, path: P) -> io::Result<usize> {
+    out.write(path.as_ref().to_str().expect("non-unicode path handling is unimplemented").as_bytes());
+}
+
 fn format_commit<W: Write>(out: &mut W, commit: Oid) -> io::Result<()> {
-    try!(out.write_all(b"pub const COMMIT: [u8; 20] = ["));
+    try!(out.write_all(b"["));
     for b in commit.as_bytes() {
         try!(write!(out, "{:#04x},", b));
     }
-    try!(out.write_all(b"];\n"));
+    out.write_all(b"]")
+}
+
+fn generate_<W: Write>(out: &mut W, repo: &Repository) -> Result<()> {
+    // Guard against repositories in initial state
+    if try!(repo.is_empty()) { return Ok(()); }
+
+    let head = try!(repo.head());
+    let commit = try!(head.resolve().chain_err(|| "couldn't resolve git HEAD")).target().unwrap();
+    try!(write!(out, "pub const COMMIT: [u8; 20] = "));
+    try!(format_commit(out, commit));
+    try!(write!(out, ";\n"));
+
+    let diff = try!(repo.diff_tree_to_workdir_with_index(Some(&repo.find_tree(repo.revparse_single("HEAD^{tree}")?.id())?), None));
+    try!(write!(out, "pub const DIRTY: bool = {};\n", diff.deltas().len() != 0));
+
     Ok(())
 }
 
@@ -27,25 +56,22 @@ pub fn generate() -> Result<()> {
     let repo = try!(Repository::open(".").chain_err(|| "couldn't open git repository"));
     let path = PathBuf::from(try!(env::var("OUT_DIR").chain_err(|| "couldn't get OUT_DIR"))).join("version.rs");
     let mut file = try!(File::create(&path).chain_err(|| format!("couldn't create {}", path.to_string_lossy())));
+    try!(generate_(&mut file, &repo));
 
-    // Guard against repositories in initial state
-    if let Ok(head) = repo.head() {
-        let commit = try!(head.resolve().chain_err(|| "couldn't resolve git HEAD")).target().unwrap();
-        try!(format_commit(&mut file, commit).chain_err(|| format!("couldn't write to {}", path.to_string_lossy())));
+    let mut paths = Vec::with_capacity(2);
+    paths.push(repo.path().join("HEAD"));
+    if let Ok(reference) = repo.head() {
+        if reference.is_branch() {
+            let name = String::from_utf8(reference.name_bytes().to_vec()).expect("non-unicode path handling is unimplemented");
+            paths.push(repo.path().join(name));
+        }
     }
-    
+
+    for path in &paths {
+        print!("cargo:rerun-if-changed=");
+        try!(write_path(&mut io::stdout(), path));
+        print!("\n");
+    }
+
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use git2::Oid;
-
-    #[test]
-    fn format_commit() {
-        let mut buf = Vec::new();
-        ::format_commit(&mut buf, Oid::from_bytes(&[0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,]).unwrap()).unwrap();
-        assert_eq!(String::from_utf8(buf).unwrap(),
-                   "pub const COMMIT: [u8; 20] = [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,];\n");
-    }
 }
